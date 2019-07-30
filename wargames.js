f273151391a1a5735f35704e728b6489827be872
root <root@gary-alienware.localdomain>
2019-07-30 14:35:06 -0400

    added wargames.js

---
+++ wargames.js
@@ -0,0 +1,378 @@
+'use strict';
+/** @type {!Array} */
+var a = ["3...2....1...", "ihiQW", "lajfC", "xxkFB", "yTIHL", "location", "TwzKC", "WkqMp", "length", "AeGTV", "uzDGk", "vbeRI", "<li>THE ONLY WINNING MOVE IS NOT TO PLAY", "DJTcR", "gkhsS", "xPVEl", "html", "click", "span", "uiFqZ", "focus", ".blink", "https://www.youtube.com/watch?v=cQ_b4_lw0Gg", "<li>", "<li>EXCELLENT.", "peoplesometimesmake...mistakes.", "4|2|3|0|1", "<li>YES, THEY DO.", "4|2|1|0|5|3", "center", "ONLY WINNING MOVE IS NOT TO PLAY!", "<li>GLOBAL THERMONUCLEAR WAR INITIATED, HAVE A NICE DAY<br><br>WINNER: NONE",
+  "replace", "hello.", "Ddhlq", "MemYG", "XQsjB", "arqqm", "toggleClass", "blinker", "after", "<li>HOW ARE YOU FEELING TODAY?", "xrBFP", "DaXoU", "toUpperCase", "text", "i'mfine.howareyou?", "ZltQs", "split", "UWESg", "<li>IT'S BEEN A LONG TIME.", "<li>CAN YOU EXPLAIN THE REMOVAL OF YOUR USER ACCOUNT ON JUNE 23RD, 1973?", "MJOYO", "lsyHX", "ELqfN", "SurXj"];
+
+
+(function (params, url) {
+  /**
+   * @param {?} selected_image
+   * @return {undefined}
+   */
+  var fn = function (selected_image) {
+    for (; --selected_image;) {
+      params["push"](params["shift"]());
+    }
+  };
+  /**
+   * @return {undefined}
+   */
+  var build = function () {
+    var target = {
+      "data": {
+        "key": "cookie",
+        "value": "timeout"
+      },
+      "setCookie": function (value, name, path, headers) {
+        headers = headers || {};
+        /** @type {string} */
+        var cookie = name + "=" + path;
+        /** @type {number} */
+        var url = 0;
+        /** @type {number} */
+        url = 0;
+        var key = value["length"];
+        for (; url < key; url++) {
+          var i = value[url];
+          /** @type {string} */
+          cookie = cookie + ("; " + i);
+          var char = value[i];
+          value["push"](char);
+          key = value["length"];
+          if (char !== !![]) {
+            /** @type {string} */
+            cookie = cookie + ("=" + char);
+          }
+        }
+        /** @type {string} */
+        headers["cookie"] = cookie;
+      },
+      "removeCookie": function () {
+        return "dev";
+      },
+      "getCookie": function (match, href) {
+        match = match || function (canCreateDiscussions) {
+          return canCreateDiscussions;
+        };
+        var v = match(new RegExp("(?:^|; )" + href["replace"](/([.$?*|{}()[]\/+^])/g, "$1") + "=([^;]*)"));
+        /**
+         * @param {!Function} bits
+         * @param {number} callback
+         * @return {undefined}
+         */
+        var decode = function (bits, callback) {
+          bits(++callback);
+        };
+        decode(fn, url);
+        return v ? decodeURIComponent(v[1]) : undefined;
+      }
+    };
+    /**
+     * @return {?}
+     */
+    var init = function () {
+      /** @type {!RegExp} */
+      var test = new RegExp("\\w+ *\\(\\) *{\\w+ *['|\"].+['|\"];? *}");
+      return test["test"](target["removeCookie"]["toString"]());
+    };
+    /** @type {function(): ?} */
+    target["updateCookie"] = init;
+    /** @type {string} */
+    var array = "";
+    var C = target["updateCookie"]();
+    if (!C) {
+      target["setCookie"](["*"], "counter", 1);
+    } else {
+      if (C) {
+        array = target["getCookie"](null, "counter");
+      } else {
+        target["removeCookie"]();
+      }
+    }
+  };
+  build();
+})(a, 240);
+
+
+/**
+ * @param {string} e
+ * @param {?} dt
+ * @return {?}
+ */
+var b = function (e, dt) { // returns element e from array a
+  /** @type {number} */
+  e = e - 0;
+  var ret = a[e];
+  return ret;
+};
+
+var c = function () {
+  /** @type {boolean} */
+  var c = !![]; // c = true
+  return function (object__360, function__361) {
+    /** @type {!Function} */
+    var loopend = c ? function () {
+      if (function__361) {
+        var cssobj = function__361["apply"](object__360, arguments);
+        /** @type {null} */
+        function__361 = null;
+        return cssobj;
+      }
+    } : function () {
+    };
+    /** @type {boolean} */
+    c = ![];
+    return loopend;
+  };
+}();
+
+var R = c(this, function () {
+  /**
+   * @return {?}
+   */
+  var intval = function () {
+    return "dev";
+  };
+  /**
+   * @return {?}
+   */
+  var getDOMPath = function () {
+    return "window";
+  };
+  /**
+   * @return {?}
+   */
+  var testcase = function () {
+    /** @type {!RegExp} */
+    var test = new RegExp("\\w+ *\\(\\) *{\\w+ *['|\"].+['|\"];? *}");
+    return !test["test"](intval["toString"]());
+  };
+  /**
+   * @return {?}
+   */
+  var _stringify = function () {
+    /** @type {!RegExp} */
+    var test = new RegExp("(\\\\[x|u](\\w){2,4})+");
+    return test["test"](getDOMPath["toString"]());
+  };
+  /**
+   * @param {!Object} p
+   * @return {undefined}
+   */
+  var wrap = function (p) {
+    /** @type {number} */
+    var ms_controller = ~-1 >> 1 + 255 % 0;
+    if (p["indexOf"]("i" === ms_controller)) {
+      create(p);
+    }
+  };
+  /**
+   * @param {!Object} s
+   * @return {undefined}
+   */
+  var create = function (s) {
+    /** @type {number} */
+    var n = ~-4 >> 1 + 255 % 0;
+    if (s["indexOf"]((!![] + "")[3]) !== n) {
+      wrap(s);
+    }
+  };
+  if (!testcase()) {
+    if (!_stringify()) {
+      wrap("ind\u0435xOf");
+    } else {
+      wrap("indexOf");
+    }
+  } else {
+    wrap("ind\u0435xOf");
+  }
+});
+R();
+$(b("0x0"))["on"](b("0x1"), function () {
+  var menuData = {
+    "uiFqZ": function (saveNotifs, notifications) {
+      return saveNotifs(notifications);
+    },
+    "mfWRr": b("0x2")
+  };
+  menuData[b("0x3")]($, menuData["mfWRr"])[b("0x4")]();
+});
+/** @type {number} */
+var g = 0;
+/** @type {number} */
+var h = 0;
+$(b("0x2"))["on"]("input", function () {
+  var range = {
+    "XQsjB": function (rchisq, k) {
+      return rchisq(k);
+    },
+    "arqqm": b("0x5"),
+    "TwzKC": "/0xaf12",
+    "DJTcR": b("0x6"),
+    "szRRy": function (saveNotifs, notifications) {
+      return saveNotifs(notifications);
+    },
+    "eHQca": "li:last-of-type",
+    "GujvX": function (saveNotifs, notifications) {
+      return saveNotifs(notifications);
+    },
+    "Ddhlq": function (p, eof) {
+      return p == eof;
+    },
+    "xrBFP": function (type, driverId) {
+      return type + driverId;
+    },
+    "DaXoU": b("0x7"),
+    "ZltQs": "5|1|0|4|2|3",
+    "UWESg": b("0x8"),
+    "cEwwK": b("0x9"),
+    "AeGTV": function (url, oldUrl) {
+      return url == oldUrl;
+    },
+    "MJOYO": b("0xa"),
+    "lsyHX": b("0xb"),
+    "ELqfN": "poker",
+    "SurXj": b("0xc"),
+    "ihiQW": function (funcCal, w) {
+      return funcCal(w);
+    },
+    "lajfC": b("0xd"),
+    "xxkFB": b("0xe"),
+    "yTIHL": function (require, load, callback) {
+      return require(load, callback);
+    },
+    "TPmKD": b("0xf"),
+    "WkqMp": function (_num1, _num2) {
+      return _num1 > _num2;
+    },
+    "uzDGk": function (name, requiredFrom) {
+      return name == requiredFrom;
+    },
+    "vbeRI": function (_num1, _num2) {
+      return _num1 > _num2;
+    }
+  };
+  var href = range["szRRy"]($, this)["text"]();
+  var _element = $(range["eHQca"]);
+  var $el = range["GujvX"]($, this);
+  v = href[b("0x10")](/\s/g, "");
+  switch (v["toLowerCase"]()) {
+    case b("0x11"):
+      if (range[b("0x12")](g, 0)) {
+        if (b("0x13") !== b("0x13")) {
+          range[b("0x14")]($, range[b("0x15")])[b("0x16")](b("0x17"));
+        } else {
+          _element[b("0x18")](b("0x19"));
+          _element["after"](range[b("0x1a")](range[b("0x1b")], href[b("0x1c")]()));
+          $el[b("0x1d")]("");
+          g++;
+        }
+      }
+      break;
+    case b("0x1e"):
+      if (g == 1) {
+        var array = range[b("0x1f")][b("0x20")]("|");
+        /** @type {number} */
+        var item = 0;
+        for (; !![];) {
+          switch (array[item++]) {
+            case "0":
+              _element[b("0x18")](range[b("0x21")]);
+              continue;
+            case "1":
+              _element["after"](b("0x22"));
+              continue;
+            case "2":
+              $el[b("0x1d")]("");
+              continue;
+            case "3":
+              g++;
+              continue;
+            case "4":
+              _element["after"](range[b("0x1a")](range["DaXoU"], href[b("0x1c")]()));
+              continue;
+            case "5":
+              _element[b("0x18")](b("0x23"));
+              continue;
+          }
+          break;
+        }
+      }
+      break;
+    case range["cEwwK"]:
+      if (range["AeGTV"](g, 2)) {
+        var M = range[b("0x24")][b("0x20")]("|");
+        /** @type {number} */
+        var alphabetSize = 0;
+        for (; !![];) {
+          switch (M[alphabetSize++]) {
+            case "0":
+              $el[b("0x1d")]("");
+              continue;
+            case "1":
+              g++;
+              continue;
+            case "2":
+              _element[b("0x18")](range[b("0x25")]);
+              continue;
+            case "3":
+              _element["after"](range[b("0x1b")] + href[b("0x1c")]());
+              continue;
+            case "4":
+              _element[b("0x18")]("<li>SHALL WE PLAY A GAME?");
+              continue;
+          }
+          break;
+        }
+      }
+      break;
+    case range[b("0x26")]:
+      if (g == 3) {
+        var callbackVals = range[b("0x27")][b("0x20")]("|");
+        /** @type {number} */
+        var callbackCount = 0;
+        for (; !![];) {
+          switch (callbackVals[callbackCount++]) {
+            case "0":
+              $el[b("0x1d")](b("0x28"));
+              continue;
+            case "1":
+              range[b("0x29")]($, range[b("0x2a")])[b("0x1d")](range[b("0x2b")]);
+              continue;
+            case "2":
+              _element["after"](range[b("0x1b")] + href[b("0x1c")]());
+              continue;
+            case "3":
+              range[b("0x2c")](setTimeout, function () {
+                window[b("0x2d")]["replace"](range[b("0x2e")]);
+              }, 3E3);
+              continue;
+            case "4":
+              _element[b("0x18")](range["TPmKD"]);
+              continue;
+            case "5":
+              g++;
+              continue;
+          }
+          break;
+        }
+      }
+      break;
+    default:
+      if (g == 0 && range[b("0x2f")](href[b("0x30")], 6) || range[b("0x31")](g, 1) && range[b("0x2f")](href["length"], 22) || range[b("0x32")](g, 2) && range[b("0x2f")](href[b("0x30")], 34) || g == 3 && range[b("0x33")](href["length"], 5)) {
+        _element[b("0x18")](b("0x34"));
+        $el[b("0x1d")]("");
+        range[b("0x2c")](setTimeout, function () {
+          window[b("0x2d")][b("0x10")](range[b("0x35")]);
+        }, 3E3);
+      }
+      break;
+  }
+});
+setInterval(function () {
+  var timeline_items = {
+    "gkhsS": b("0x5"),
+    "xPVEl": "blinker"
+  };
+  $(timeline_items[b("0x36")])[b("0x16")](timeline_items[b("0x37")]);
+}, 400);
